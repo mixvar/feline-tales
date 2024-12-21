2b987bab6d05e6247d7451d2f40924e3efc73871
Mikołaj Klaman <mixvar@gmail.com>
2024-12-21 14:35:48 +0100

    feat: add basic auth and user widget

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,28 @@ export default {
       serif: ['Spectral'],
       cursive: ['Alex Brush'],
     },
-    colors: {
-      felineGreen: {
-        light: '#75a185',
-        DEFAULT: '#5d826a',
-        dark: '#3a5945',
-      },
-      felineOrange: {
-        light: '#f7a45c',
-        DEFAULT: '#ec8e3e',
-        dark: '#a15412',
-      },
-      felineLove: {
-        light: '#fc876a',
-        DEFAULT: '#ef6c4a',
-        dark: '#a13113',
-      },
-      felineBg: {
-        light: '#f3dfad',
-        DEFAULT: '#beb9a2',
-        dark: '#706c55',
+    extend: {
+      colors: {
+        felineGreen: {
+          light: '#75a185',
+          DEFAULT: '#5d826a',
+          dark: '#3a5945',
+        },
+        felineOrange: {
+          light: '#f7a45c',
+          DEFAULT: '#ec8e3e',
+          dark: '#a15412',
+        },
+        felineLove: {
+          light: '#fc876a',
+          DEFAULT: '#ef6c4a',
+          dark: '#a13113',
+        },
+        felineBg: {
+          light: '#f3dfad',
+          DEFAULT: '#beb9a2',
+          dark: '#706c55',
+        },
       },
     },
   },
